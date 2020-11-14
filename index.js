6bf7901670971c0c5763aa0665fbb3ff8da9de9f
Ireniushka <irenerengelruiz@gmail.com>
2020-11-14 18:37:07 +0100

    ejercicio realizado

--- index.js
+++ index.js
@@ -3,48 +3,6 @@
 
 // Como se puede apreciar en la imagen, cada elemento que esté en una posición par de de la lista tiene que tener la clase "color-item--odd". Esta clase debe añadirse desde javascript, NO haciendo uso del selector css nth-of-type(odd) o similares. NOTA: En este caso vamos a considerar un elemento par pensando en el primer elemento como el 1 no como el 0.
 
-let list = document.getElementsByClassName("color-list")[0];
-
-function addElementsToList(lista){
-
-  for(let i= 0; i<colorList.length; i++){
-    let li = document.createElement("li");
-
-    if(i%2 ==1) li.classList.add("color-item--odd");
-
-    let div1 = document.createElement("div");
-    div1.classList.add("color-name");
-    div1.innerText= colorList[i].colorName;
-    div1.addEventListener("click", ()=>clickDiv(colorList[i].colorName, Event));
-    li.append(div1);
-
-    let div2 = document.createElement("div");
-    div2.classList.add("color-show");
-    div2.innerText="Muestra";
-    div2.style.backgroundColor = colorList[i].hex;
-    li.append(div2);
-
-    let button1 = document.createElement("button");
-    button1.classList.add("color-set");
-    button1.innerText = "Next Item Color";
-    button1.addEventListener("click", ()=> clickBtn1(colorList[i].hex, colorList[i].colorName, Event), false);
-    li.append(button1);
-
-    let button2 = document.createElement("button");
-    button2.classList.add("color-set");
-    button2.innerText = "Page Color";
-    button2.addEventListener("click", ()=> clickBtn2(colorList[i].hex, Event), false);
-    li.append(button2);
-
-    lista.append(li);
-  }
-
-  
-
-}
-
-addElementList(list);
-
 // Cada elemento del listado contendrá:
 //    * El nombre del color.
 //    * Una muestra en la que se ve el color.
@@ -102,3 +60,75 @@ const colorList = [
     hex: '#c0c0c0'
   }
 ];
+
+let list = document.getElementsByClassName("color-list").item(0);
+
+function addElementsToList(lista){
+
+  for(let i= 0; i<colorList.length; i++){
+    let li = document.createElement("li");
+    li.classList.add("color-item");
+
+    if(i%2 ==1){
+      li.classList.add("color-item--odd");
+    }
+
+    let div1 = document.createElement("div");
+    div1.classList.add("color-name");
+    div1.innerText= "Color: "+colorList[i].colorName;
+    div1.addEventListener("click", ()=>clickDiv(colorList[i].colorName, event, true));
+    li.append(div1);
+
+    let div2 = document.createElement("div");
+    div2.classList.add("color-show");
+    div2.innerText="Muestra";
+    div2.style.backgroundColor = colorList[i].hex;
+    div2.addEventListener("click", ()=>clickDiv(colorList[i].colorName, event, true));
+    li.append(div2);
+
+    let button1 = document.createElement("button");
+    button1.classList.add("color-set");
+    button1.innerText = "Next Item Color";
+    button1.addEventListener("click", ()=> clickBtn1(colorList[i].colorName, colorList[i].hex, event), false);
+    li.append(button1);
+
+    let button2 = document.createElement("button");
+    button2.classList.add("color-set");
+    button2.innerText = "Page Color";
+    button2.addEventListener("click", ()=> clickBtn2(colorList[i].hex, event), false);
+    li.append(button2);
+
+    lista.append(li);
+  }
+
+  let clickDiv = (color, event)=> {
+    event.stopPropagation();
+    alert("Pulsaste en el color: " + color);
+  };
+  
+  let clickBtn1 = (name, color, event) => {
+    event.stopPropagation();
+    let listChildren = lista.children;
+    for(let i =1; i<listChildren.length; i++){
+      let child = listChildren.item(i);
+      if(child.getElementsByTagName("div").item(0).textContent == "Color: "+ name){
+        i == colorList.length? 
+          child=listChildren.item(0).style.backgroundColor = color:
+          child.nextSibling.style.backgroundColor = color;
+      }
+    }
+  };
+
+  let clickBtn2 = (color, event) => {
+    event.stopPropagation();
+    document.getElementsByTagName("body").item(0).style.backgroundColor = color;
+
+  }
+
+  let clickBody = () => alert("body");
+
+  document.getElementsByTagName("body").item(0).addEventListener("click", clickBody, false);
+
+}
+
+addElementsToList(list);
